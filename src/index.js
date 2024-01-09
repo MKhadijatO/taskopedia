a0593de6479911d775b8c1eaf1928dba247a2569
Khadijat <muhammadkhadijato@gmail.com>
2024-01-09 23:10:00 +0100

    setup 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from 'react'; //not needed for react v18
 import ReactDOM from 'react-dom/client';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-   
-  </React.StrictMode>
+  <div>Hello!</div>
 );
 
