fd554e62a542305e41f5129a295722e33d051bad
Khadijat <muhammadkhadijato@gmail.com>
2024-01-11 14:26:42 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from 'react'; //not needed for react v18
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div>Hello!</div>
-);
+// root.render(
+//   <div>Hello!</div>
+// );
 
+// function MainBody() {}
+root.render(
+  <div>
+    <p>In this course, we will learn react js by building Taskopedia</p>
+    <ul>
+      <li>Send details</li>
+      <li>Contact the seamstress</li>
+      <li>Watch the trouser video</li>
+    </ul>
+  </div>
+);
