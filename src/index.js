f88dc09062b19ffebdc4a0c0d267c5214eec770a
Khadijat <muhammadkhadijato@gmail.com>
2024-01-11 14:33:10 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
 //   <div>Hello!</div>
 // );
 
-// function MainBody() {}
-root.render(
-  <div>
-    <p>In this course, we will learn react js by building Taskopedia</p>
-    <ul>
-      <li>Send details</li>
-      <li>Contact the seamstress</li>
-      <li>Watch the trouser video</li>
-    </ul>
-  </div>
-);
+function MainBody() {
+  //MainBody is written in pascal case. All react components are written in PASCAL CASE.
+  return (
+    <div>
+      <p>In this course, we will learn react js by building Taskopedia</p>
+      <ul>
+        <li>Send details</li>
+        <li>Contact the seamstress</li>
+        <li>Watch the trouser video</li>
+      </ul>
+    </div>
+  );
+}
+root.render(MainBody())
