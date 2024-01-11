77407ba0485c023103c52302a507d21f7de4f9ad
Khadijat <muhammadkhadijato@gmail.com>
2024-01-11 14:53:28 +0100

    update; task

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 //   <div>Hello!</div>
 // );
 
+function Header(){
+  return (
+    <h1>REACT COURSE</h1>
+  )
+}
+
 function MainBody() {
   //MainBody is written in pascal case. All react components are written in PASCAL CASE.
   return (
@@ -19,10 +25,17 @@ function MainBody() {
     </div>
   );
 }
+
+function Footer(){
+  return(
+    <p>Happy Coding</p>
+  )
+}
 root.render(
   <div>
+    <Header />
     <MainBody />
-    <MainBody />
-    <p>Thank you.</p>
+    <Footer/>
   </div>
 ); // The react '.render' can only accept one main wrapper parent element i.e the div element in this code wraps the mainbody and the p element in order for it to work properly.
+
