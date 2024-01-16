eaaee57d4b2b99cb7070439aa2a7c87392e841ca
Khadijat <muhammadkhadijato@gmail.com>
2024-01-17 00:21:18 +0100

    updated

--- src/MainBody.jsx
+++ src/MainBody.jsx
@@ -2,9 +2,11 @@ export default function MainBody() {
     const courseTopic = "React Js";
     const totalLecture = 2;
     return (
-      <div>
-        <p>In this course, we will learn {courseTopic} by building Taskopedia</p>
-        <p>Total Lecture - {totalLecture}</p>
+      <div style={{ minHeight:"70vh" }}>
+        <p>In this course, we will learn {courseTopic} by building Taskopedia
+        <br/>
+        Total Lecture - {totalLecture}
+        </p>
         <ul>
           <li>Basic Foundation</li>
           <li>Functional and Class Components</li>
